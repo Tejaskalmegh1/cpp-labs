5e018a3e50cc199d5494526234f9b87421f43a1d
tejaskalmegh1 <tejaskalmegh123@gmail.com>
2026-05-19 23:18:08 +0530

    feat: add intro to data types and variables in C++

---
+++ fundamentals/data_types.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int age = 21;
+    float height = 5.9f;
+    double salary = 50000.75;
+    char grade = 'A';
+    bool isCppEasy = true;
+
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Salary: " << salary << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "C++ Easy: " << isCppEasy << endl;
+
+    return 0;
+}
---
+++ fundamentals/variables.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int age = 21;
+    int year = 2026;
+    float height = 5.9f;
+    double salary = 75000.50;
+    char grade = 'A';
+    bool isCppFun = true;
+    const double PI = 3.14159;
+
+    cout << "Age: " << age << endl;
+    cout << "Year: " << year << endl;
+    cout << "Height: " << height << endl;
+    cout << "Salary: " << salary << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Is C++ Fun? " << isCppFun << endl;
+    cout << "PI Value: " << PI << endl;
+
+    return 0;
+}
