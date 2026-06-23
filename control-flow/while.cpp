f4e0fa966fa123a50b8dc82f7f64dae6121481ec
tejaskalmegh1 <tejaskalmegh123@gmail.com>
2026-06-23 21:44:16 +0530

    feat: add while loop in cpp

---
+++ control-flow/while.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+    int i = 1;
+
+    // while loop is used to execute a block of code repeatedly while a condition remains true
+
+    while(i <= 5){
+        cout<<"\n this is while loop..";
+        i++;
+    }
+    
+    return 0;
+}
