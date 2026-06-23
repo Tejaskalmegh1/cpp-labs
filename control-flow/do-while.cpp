1860e5ac430e63100962ec7a2faf19a27f70e7a2
tejaskalmegh1 <tejaskalmegh123@gmail.com>
2026-06-23 21:49:57 +0530

    feat: add do while loop in cpp

---
+++ control-flow/do-while.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+    int i = 1;
+
+    // do-while loop executes a block of code at least once and then repeats it while the condition is true
+
+    do {
+      cout<<"\n this is do while loop...";
+      i++;
+    } while ( i <= 5 );
+    
+    
+    return 0;
+}
