97a385d7e01f54f06e3e2f3cc547128327ef2db4
tejaskalmegh1 <tejaskalmegh123@gmail.com>
2026-06-23 21:40:16 +0530

    feat: add for loop in cpp

---
+++ control-flow/for.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int i;
+
+    // for loop is used to repeat a block of code a fixed number of times
+    
+    for ( i = 1; i < 11; i++) {
+        cout<<"\n Hello, welcome to c++";
+    }
+    
+    return 0;
+}
